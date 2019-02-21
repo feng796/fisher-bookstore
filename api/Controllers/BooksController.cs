18d81a463a9151f207bef2919794b4e747bec85a
Wenyi Feng <feng.796@osu.edu>
2019-02-21 00:17:36 -0500

    send PUT on Postman

--- api/Controllers/BooksController.cs
+++ api/Controllers/BooksController.cs
@@ -50,7 +50,7 @@ namespace Fisher.Bookstore.Api.Controllers
             return Ok(db.Books);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetBook")]
         public IActionResult GetBook(int id)
         {
             var book = db.Books.FirstOrDefault(b => b.Id == id);
@@ -60,6 +60,44 @@ namespace Fisher.Bookstore.Api.Controllers
                 return NotFound();
             }
             return Ok(book);
+        }
+
+        public IActionResult Post([FromBody]Book book)
+        {
+            if(book == null)
+            {
+                return BadRequest();
+            }
+
+            db.Books.Add(book);
+            db.SaveChanges();
+
+            return CreatedAtRoute("GetBook", new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Book book)
+        {
+            //validate the incoming book
+            if (book == null || book.Id != id)
+            {
+                return BadRequest();
+            }
+
+            // verity the book is in the database
+            var bookToEdit = db.Books.FirstOrDefault(b => b.Id == id);
+            if(bookToEdit == null)
+            {
+                return NotFound();
+            }
+
+            bookToEdit.Title = book.Title;
+            bookToEdit.ISBN = book.ISBN;
+
+            db.Books.Update(bookToEdit);
+            db.SaveChanges();
+
+            return NoContent();
         }
     }
 }
