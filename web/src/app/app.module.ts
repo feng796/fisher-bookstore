6cb038c0e0a12479dae8514c9b772b25a543c97e
Wenyi Feng <feng.796@osu.edu>
2019-03-15 17:08:20 -0700

    finish

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books/books.component';
+import { BookRowComponent } from './books/book-row/book-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BooksComponent,
+    BookRowComponent
   ],
   imports: [
     BrowserModule,
