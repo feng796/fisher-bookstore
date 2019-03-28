6fe2a2362877ed502779acdb697093fe5ac9d054
Wenyi Feng <feng.796@osu.edu>
2019-03-27 21:40:49 -0400

    lab 8

--- api/Models/Book.cs
+++ api/Models/Book.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Fisher.Bookstore.Models
 {
@@ -13,6 +14,8 @@ namespace Fisher.Bookstore.Models
         public string ISBN { get; set; }
 
         public DateTime publicationDate { get; set; }
+
+        public string Publisher { get; set;}
 
     }
 }
